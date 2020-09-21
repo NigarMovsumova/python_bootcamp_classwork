89b82656fe95f1c3c8cd2ae562ab175775ace6c2
Nigar <movsum.nigar@gmail.com>
2020-09-21 17:49:20 +0400

    added lesson materials

--- Print Function/Separator.py
+++
@@ -1,8 +0,0 @@
-print("Name\tSurname\temail\tPhone\tAddress\t")
-
-print("Name", "Surname", "email", "Phone", "Address", sep="\t")
-
-print("Name", "Surname", "email", "Phone", "Address", sep="-")
-
-print("Name", "Surname", "email", "Phone", "Address", sep="__")
-
