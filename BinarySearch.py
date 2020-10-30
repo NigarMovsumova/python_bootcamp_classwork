cd3da3ead8aa2258d958554ff870e96a8c9e1593
Nigar <movsum.nigar@gmail.com>
2020-10-30 20:21:14 +0400

    reformatted code

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,10 +1,12 @@
 from random import randint
 
+
 def create_randomized_list():
     numbers = []
     for i in range(0, 10):
         numbers.append(randint(0, 5))
     return numbers
+
 
 numbers = create_randomized_list()
 print(numbers)
@@ -17,11 +19,11 @@ left = 0
 right = len(numbers) - 1
 
 while numbers[middle] != target and left <= right:
-        if target > numbers[middle]:
-            left = middle + 1
-        else:
-            right = middle - 1
-        middle = (left + right) // 2
+    if target > numbers[middle]:
+        left = middle + 1
+    else:
+        right = middle - 1
+    middle = (left + right) // 2
 
 if left > right:
     print('Number is not found')
