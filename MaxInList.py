d30fd21d8943f7428887d8b6dd34cd79d6e447ea
Movsumova Nigar <n.movsumova@itstep.edu.az>
2020-11-01 15:36:41 +0400

    added new task

---
+++ MaxInList.py
@@ -0,0 +1,8 @@
+# list
+# listde dublikatlar ola biler
+# random olmasin
+# static olsun list
+# max-umu tapirsiniz ve onun listde
+# neche defe tekrarlandigini tapirsiniz.
+
+numbers = [1, 2, 3, 4]
