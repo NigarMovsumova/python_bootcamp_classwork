90fd07f79db32007496e3f459a98ee099e66de80
Movsumova Nigar <n.movsumova@itstep.edu.az>
2020-11-15 17:10:35 +0400

    added Students tasks explanation without refactoring

--- students/Students.py
+++ students/Students.py
@@ -4,19 +4,62 @@ import pickle
 # filedan melumatlari oxuyub, sort edib yeniden,
 # evvelki melumatlari silib filedan,
 # file-a yazmaq gerekir.
-sample = {
-    'name': 'Python'
+from collections import OrderedDict
+
+
+students = {
+    1: {
+        'firstname':'Ali',
+        'lastname':'Abbasov',
+        'subject':'Computer Science',
+        'mark': 12,
+        'age': 26
+    },
+    2: {
+        'firstname': 'Aysha',
+        'lastname':'Movsumova',
+        'subject':'Machine Learning',
+        'mark': 3.7,
+        'age': 17
+    },
+    3: {
+        'firstname': 'Nigar',
+        'lastname':'Movsumova',
+        'subject':'Machine Learning',
+        'mark': 3.7,
+        'age': 39
+    }
 }
 
 file = open("students.txt", "wb")
-pickle.dump(sample, file)
+pickle.dump(students, file)
 file.close()
 
 file = open('students.txt', 'rb')
 loaded_dictionary = pickle.load(file)
 file.close()
 
-print(loaded_dictionary)
+dictionary_sorted = sorted(students.items(), key=lambda x: (x[1]['age']), reverse=True)
+
+print(dictionary_sorted)
+
+file = open('students.txt', 'wb')
+file.truncate()
+file.close()
+
+# file = open('students.txt', 'rb')
+# file_content = pickle.load(file)
+# print(file_content)
+# file.close()
+
+file = open('students.txt', 'wb')
+pickle.dump(dictionary_sorted, file)
+file.close()
+
+file = open('students.txt', 'rb')
+print(pickle.load(file))
+file.close()
+
 
 
 
