5e5074564da06c16f357f71329ee3c4645f2c15e
Nigar <movsum.nigar@gmail.com>
2020-11-17 00:00:22 +0400

    added AlphabetRecursion explanation

--- AlphabetRecursion.py
+++ AlphabetRecursion.py
@@ -1,4 +1,19 @@
 # a - z
 # a b c d e - z
 # z y x - a
+#
+# print(ord('c'))
+# print(chr(97))
 
+# print(chr(122))
+# print(chr(123))
+
+
+def print_alphabet(character_index):
+    if character_index == 123:
+        return
+    print(chr(character_index))
+    return print_alphabet(character_index + 1)
+
+
+print_alphabet(97)
