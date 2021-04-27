4001040f65c962dd2401212492e9c090df0f7a5a
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-04-27 20:07:23 +0400

    added operators' explanation

---
+++ operators.py
@@ -0,0 +1,48 @@
+print(5**2)
+print(10**-1)
+print(10 // 3)
+print(15 // 6)
+
+salary = 1000
+# salary = salary + 500
+salary += 500
+# salary = salary - 500
+salary -= 500
+# salary = salary / 3
+salary /= 3
+# salary = salary * 5
+salary *= 5
+
+print(salary)
+print(salary == 1500)
+print(salary != 1500)
+salary = 2000
+print(salary <= 2000)
+print(salary >= 2000)
+print(salary < 2000)
+print(salary > 2000)
+bonus = 700
+print(bonus > 500 and salary > 1500)
+print(bonus < 400 and salary > 1500)
+print(bonus > 500 or salary > 1500)
+print(bonus < 500 or salary < 1500)
+print(not True)
+print(bonus > 500)
+print(not bonus > 500)
+print(not bonus < 500)
+
+print((5 > 7 and 14 > 7)
+      or
+      (12 > 7))
+
+print(int(True))
+print(int(False))
+print(0 and 7 > 5)
+print(100 and 50 and 150)
+print(not 5 and 7 > 5)
+print(not 0 and 7 > 5)
+print(not 1 and 7 > 5)
+# print(100 and 50 and 150 and True and 0)
+# print((not 1 and 7 > 5) == True)
+
+
---
+++ practise.py
@@ -0,0 +1,4 @@
+# İstifadəçiyə 1 sual verilir.
+# Onun cavabını daxil etdikdən sonra,
+# siz cavabın doğruluğunu yoxlayıb
+# ekranda əks etdirəcəksiniz.
