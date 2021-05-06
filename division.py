18d0a88f2f43b4fc832f258a8adb9ec7d9247a3a
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-05-06 21:14:34 +0400

    added new task for cycles

---
+++ division.py
@@ -0,0 +1,2 @@
+# 1-100 araliq. 2, 3, 5, 7ye ayriliqda bolunen
+# ededlerin cemini hesablamaq gerekir.
--- practise.py
+++ practise.py
@@ -1,1 +1,9 @@
 # 2. 1den 100e kimi 2ye bolunenlerin sayini tapmaq gerekir
+
+index = 1
+count = 0
+while index <= 100:
+    index += 1
+    if index % 2 == 0:
+        count += 1
+print(count)
