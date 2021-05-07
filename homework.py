69ac8644c64e675035eca62c96ef91b149f56b06
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-05-07 21:58:19 +0400

    added homework

--- homework.py
+++ homework.py
@@ -1,14 +1,13 @@
-# 4) İstifadəçi ədəd daxil edir,
-# bu ədədin rəqəmlərinin artan ardıcıllıqla
-# olub olmamasını yoxlayan proqram yazın.
-# (12299 artan ardıcıllıq, 122044 artan ardıcıllıq deyil)
+# MultiplicationTable
 
-# 0. previous_digit = 10
-# 1. boolean (bool) flag = True
-# 2. 5 defe -
-# 3. digit = number % 10
-# 4. eger digit previous_digitden ashagidirsa ->
-# 5. flag = False,  break
-# 6. previous_digit = number % 10
-# 7. number // 10
-print(len(str(123)))
+# https://www.python.org/dev/peps/pep-0008/
+
+# 1. İstifadəçi diapozonu daxil edir və
+# proqram həmin diapozonda vurma cədvəlini konsola çıxarır.
+# Məsələn, əgər istifadəçi 3 və 5 daxil edərsə,
+# bu formada çap edilməlidir.
+# 3*1 = 3 3*2 = 6 3 * 3 = 9 ... 3 * 10 = 30
+# ...
+# 5*1 = 5 5 *2 = 10 5 * 3 = 15 ... 5 * 10 = 50
+
+# 2.
--- zerodivision.py
+++ zerodivision.py
@@ -1,8 +1,20 @@
-number = int(input('Enter the number: '))
+# number = int(input('Enter the number: '))
+#
+# try:
+#     print(number / 0)
+# except ZeroDivisionError:
+#     print('You can not divide by zero.')
 
+
+dividend = None
+divider = None
 try:
-    print(number / 0)
-except ZeroDivisionError:
-    print('You can not divide by zero.')
-
-
+    dividend = int(input('Enter the dividend: '))
+    divider = int(input('Enter the divider: '))
+except ValueError:
+    print('You should enter a valid number.')
+else:
+    try:
+        print(dividend / divider)
+    except ZeroDivisionError:
+        print('You can not divide by zero.')
