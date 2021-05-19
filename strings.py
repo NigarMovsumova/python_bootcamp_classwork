f224075bbf8a9cfaa4fcbeaf29c14fb9b30c0f9a
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-05-19 22:06:05 +0400

    added strings explanation

---
+++ strings.py
@@ -0,0 +1,103 @@
+# print('hello')
+# print(ord('!'))
+# print(ord('a'))
+# print(ord('A'))
+# print(ord('['))
+
+# table = 'table'
+
+# for character in table:
+#     print(character)
+#
+# print(len(table))
+#
+# print('t' in table)
+# print('y' in table)
+# print('1' in table)
+# print('ta' in table)
+# print('tb' in table)
+# print('t' in table and 'b' in table)
+#
+# print(table[0])
+# print(table[1])
+# print(table[0:10])
+# print(table[len(table) - 1])
+# print(table[len(table) - 2])
+# print(table[-1])
+# print(table[-2])
+# print(table[-3])
+#
+# computer = 'computer'
+# print(computer[2:])
+# # 'computer' -> com
+# print(computer[:3])
+# print(computer[:])
+#
+# print(computer[-1])
+# print(computer[-2])
+# # print(computer[0])
+# print(computer[-8])
+
+# for i in range(-1, -1 * (len(computer) + 1), -1):
+#     print(computer[i])
+
+# 9 -> len(computer) + 1
+# i = -1
+# while i > -1 * (len(computer) + 1):
+#     print(computer[i])
+#     i -= 1
+
+# fly = 'FLY'
+# print(fly.lower())
+# car = 'car'
+# print(car.upper())
+# lipsum = '''          Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit, sed do eiusmod
+# tempor incididunt ut labore et dolore magna aliqua.
+# Ut enim ad minim veniam, quis nostrud exercitation
+# ullamco laboris nisi ut aliquip ex ea commodo consequat.
+# Duis aute irure dolor in reprehenderit in voluptate velit
+# esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+# occaecat cupidatat non proident, sunt in
+# culpa qui officia deserunt mollit anim id est laborum.      '''
+# print(lipsum)
+# print(lipsum.strip())
+# print(lipsum.replace(' ', '*'))
+# fullname = 'Nigar' + ' Movsumova'
+# print(fullname)
+#
+# fullname = 'Nigar'
+#
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# example = 'My name is {0}, and I am {1}'
+# print(example.format(age, name))
+# example = 'My name is {name}, and I am {age}'
+# print(example.format(name=name, age=age))
+# print(example.format(age=name, name=age))
+#
+# sample_txt = '''
+# My name is {name}.
+# My surname is {surname}.
+# I am {age}.
+# I was born in {birth_year} in {birth_place}.
+# My address is {address}. My phone number is {phone_number}.'''
+#
+
+example = 'Sorry1994'
+print(example[0].isupper())
+print(example[0].islower())
+print(example[0].isdigit())
+print(example.isalnum())
+print(example.istitle())
+
+# escape sequence
+print('example\nexample')
+print('example\\')
+print('example\\\\')
+print('\'')
+print('\"')
+print('\tMy name is Nigar.\nToday is a great day.')
+
+# C:\xampp\install
+
