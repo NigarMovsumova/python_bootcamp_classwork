bdd2f2f843c7a995f1245a830f73a33a9b6c827f
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-05-20 19:31:50 +0400

    added lists task

---
+++ lists.py
@@ -0,0 +1,32 @@
+students = ['Rena', 'Nigar', 'Nuray', 'Nigar',
+            'Shakhhuseyn', 'Yusif', 'Murad',
+            'Seymur', 'Zamin', 'Sabuhi',
+            'Elmar']
+
+# for student in students:
+#     print(student)
+
+print(students[-1])
+print(students[0])
+students[1] = 'Armudu Stekan'
+text = 'test text'
+print(text[0])
+print(text[4])
+print(text[0])
+print(students[0:4])
+print(students[-5:-1])
+print(students[-5:])
+print(students[4:])
+print(len(students))
+students.append('Lidiya')
+students.append('Farize')
+students.append('Cume')
+students.append('Nelbeki')
+students.append('Tukezban')
+print(students)
+
+# Siyahı verilir. Siyahıda x elementi var.
+# Həmin elementi istifadechinin daxil etdiyi
+# sətrlə əvəz edin.
+
+
