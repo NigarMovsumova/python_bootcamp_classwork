11d9f19ef0f3e6a3d7642d33bc6cff1edf6ac326
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-05-20 21:04:21 +0400

    added cars practise task explanation

---
+++ cars.py
@@ -0,0 +1,12 @@
+cars = ['BMW', 'Rolls Royce', 'Bentley', 'Kamaz', 'Lada Kalina', 'Tesla']
+scores = [4.3, 4.7, 4.8, 2.3, -1.6, 5]
+
+# BMW - 4.3
+# Rolls Royce - 4.7
+# Bentley - 4.8
+# Tesla - 5
+
+# 1. eger 4den yuxaridirsa ve ya beraberdirse score,
+# mashin adi + meslehetli oldugu qeyd olunsun
+# 2. eger neqativdirse, - tehlukelidir
+# 3. eger 0 - 4 arasindadirsa - meslehet deyil.
--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-students = ['Rena', 'Nigar', 'Nuray', 'Nigar',
+students = ['Rena', 'Nigar', 'Nigar', 'Nuray', 'Nigar',
             'Shakhhuseyn', 'Yusif', 'Murad',
             'Seymur', 'Zamin', 'Sabuhi',
             'Elmar']
@@ -6,27 +6,58 @@ students = ['Rena', 'Nigar', 'Nuray', 'Nigar',
 # for student in students:
 #     print(student)
 
-print(students[-1])
-print(students[0])
-students[1] = 'Armudu Stekan'
-text = 'test text'
-print(text[0])
-print(text[4])
-print(text[0])
-print(students[0:4])
-print(students[-5:-1])
-print(students[-5:])
-print(students[4:])
-print(len(students))
-students.append('Lidiya')
-students.append('Farize')
-students.append('Cume')
-students.append('Nelbeki')
-students.append('Tukezban')
-print(students)
+# print(students[-1])
+# print(students[0])
+# text = 'test text'
+# print(text[0])
+# print(text[4])
+# print(text[0])
+# print(students[0:4])
+# print(students[-5:-1])
+# print(students[-5:])
+# print(students[4:])
+# print(len(students))
+# students.append('Lidiya')
+# students.append('Farize')
+# students.append('Cume')
+# students.append('Nelbeki')
+# students.append('Tukezban')
+# print(students)
 
 # Siyahı verilir. Siyahıda x elementi var.
 # Həmin elementi istifadechinin daxil etdiyi
 # sətrlə əvəz edin.
 
+characters = ['word', 'x', 'x', 'x', 'x']
+# test_input = input('Enter the word to replace with:')
+# index = 0
+# counter = 0
+# for character in characters:
+#     if character == 'x':
+#         counter += 1
+#         characters[index] = test_input
+#     if counter == 2:
+#         break
+#     index += 1
+#
+# print(characters)
 
+print('t' in 'table')
+print('Elmar' in students)
+print('Sabina' in students)
+print(len(students))
+students.remove('Nigar')
+print(students)
+students.insert(7, 'Baxtiyar')
+print(students)
+# students.append(77)
+# students.append(True)
+# students.append(7.876)
+print(students)
+index = 0
+
+numbers = [77, 777, 7]
+students += numbers
+students = students + numbers
+print(students + numbers)
+print(students)
