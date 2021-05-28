0d5e4a8083d3b23fc80eafcaab1f206a351dae46
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-05-28 21:53:40 +0400

    added daltonism explanation

--- daltonism.py
+++ daltonism.py
@@ -8,6 +8,38 @@
 # Eger 3 duzdurse, daltonizm elametleri oldugu qeyd olunmalidir.
 # Eger 0, 1 ve ya 2 duz cavab veribse, daltonik oldugu
 # qeyd olunmalidir.
+from tkinter import *
+
+
+def check_answer(user_answer, correct_answer):
+    print(user_answer, '  ', correct_answer)
+    if user_answer == correct_answer:
+        print('Answer is correct')
+    else:
+        print('Answer is not correct')
+
+
+root_window = Tk()
+root_window.title('Game App')
+root_window.geometry('500x500')
+# fg = foreground color
+# bg = background color
+label = Label(text='', bg='black', height=3, width=50)
+label.pack()
+
+red_button = Button(text='Red', width=50, height=1, command=
+                    lambda: check_answer('red', label.cget("bg")))
+red_button.pack()
+
+green_button = Button(text='Green', width=50, height=1,
+                      command=lambda: check_answer('green', label.cget("bg")))
+green_button.pack()
+
+black_button = Button(text='Black', width=50, height=1,
+                      command=lambda: check_answer('black', label.cget("bg")))
+black_button.pack()
+
+root_window.mainloop()
 
 
 
--- test.py
+++ test.py
@@ -1,29 +1,23 @@
 from tkinter import *
-def print_to_terminal():
-    admin_username = 'NigarMovsum'
-    admin_password = 'Step2020!'
-    # print('Username =', username.get())
-    # print('Password =', password.get())
-    if username.get() == admin_username and admin_password == password.get():
-        successful_login_label = Label(text='You are successfully logged in')
-        successful_login_label.pack()
-    else:
-        unsuccessful_login_label = Label(text='Username or Password is wrong')
-        unsuccessful_login_label.pack()
 root_window = Tk()
-root_window.title('Game App')
-root_window.geometry('500x500')
-# fg = foreground color
-# bg = background color
-label = Label(text='Welcome to the Game App:', font=('Arial', 25),
-              fg='red', bg='black')
+root_window.title("Daltonism")
+root_window.geometry("400x400")
+label = Label(text="Welcome", bg="grey", font=("Times New Roman", 20))
 label.pack()
-username = Entry()
-username.insert(0, 'Enter the username')
-username.pack()
-password = Entry(show="*")
-password.insert(0, 'Enter the password')
-password.pack()
-button = Button(text='Press Me!', command=print_to_terminal, height=15, width=15)
+label = Label(text="1.             ", bg="green")
+label.pack()
+label = Label(text="2.             ", bg="red")
+label.pack()
+label = Label(text="3.             ", bg="purple")
+label.pack()
+label = Label(text="4.             ", bg="yellow")
+label.pack()
+button = Button(text="1.Button", bg="green")
+button.pack()
+button = Button(text="2.Button", bg="red")
+button.pack()
+button = Button(text="3.Button", bg="purple")
+button.pack()
+button = Button(text="4.Button", bg="yellow")
 button.pack()
 root_window.mainloop()
