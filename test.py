26a09021dd7208e43c5a834828f4c7b4203eba33
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-05-28 20:12:57 +0400

    added tkinter practise task

--- test.py
+++ test.py
@@ -1,22 +1,15 @@
 from tkinter import *
-
-
-def enter_password(event):
-    password.delete(0, len(password.get()))
-    password.config(show='*')
-
-
-def login():
+def print_to_terminal():
     admin_username = 'NigarMovsum'
     admin_password = 'Step2020!'
+    # print('Username =', username.get())
+    # print('Password =', password.get())
     if username.get() == admin_username and admin_password == password.get():
         successful_login_label = Label(text='You are successfully logged in')
         successful_login_label.pack()
     else:
         unsuccessful_login_label = Label(text='Username or Password is wrong')
         unsuccessful_login_label.pack()
-
-
 root_window = Tk()
 root_window.title('Game App')
 root_window.geometry('500x500')
@@ -25,26 +18,12 @@ root_window.geometry('500x500')
 label = Label(text='Welcome to the Game App:', font=('Arial', 25),
               fg='red', bg='black')
 label.pack()
-username = Entry(width=50)
-# username.insert(0, 'trololo')
+username = Entry()
 username.insert(0, 'Enter the username')
 username.pack()
-
-password = Entry(width=50)
+password = Entry(show="*")
 password.insert(0, 'Enter the password')
-password.bind('<FocusIn>', enter_password)
-
 password.pack()
-
-button = Button(text='Press Me!', command=login, width=50, height=1)
+button = Button(text='Press Me!', command=print_to_terminal, height=15, width=15)
 button.pack()
-
 root_window.mainloop()
-
-# 1. Tkinterde password-u baglamaq
-# 2. Entrynin ve Buttonun olchulerini deyishmek
-# 3. Usernamedede ele etmek lazimdir, men uzerine basan kimi
-# username fieldin yazilar pozulsun.
-# 4. Her defe yeni label yaradilmasin, eyni label-in
-# yazisi deyishsin.
-
