018882434d95d6fa9776076c11b9856ee863021d
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-06-01 20:57:51 +0400

    added username task file explanation

--- files.py
+++ files.py
@@ -5,12 +5,26 @@ file = open("users", "r")
 # print(file_content)
 
 line = file.readline()
-# print(len('    nigarm       '))
-# print(len('    nigarm       '.strip()))
+print(len('    nigarm       '))
+print(len('    nigarm       '.strip()))
+flag = False
 while line:
     if username == line.strip():
-        print('Users exists!!!')
+        flag = True
         break
     line = file.readline()
 
 file.close()
+
+# print('User exists ? ', flag)
+
+# if flag:
+#     print('User exists!')
+# else:
+#     print('User does not exist!')
+
+if not flag:
+    print('User does not exist!')
+else:
+    print('User exists')
+
