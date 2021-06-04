13de3a45c2bb248a0cd459b215f39a9a4df7bd24
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-06-04 20:07:19 +0400

    added random practise task

---
+++ random-module.py
@@ -0,0 +1,31 @@
+import random
+
+
+# def get_positive_numbers(numbers):
+#     positive_numbers = []
+#     for number in numbers:
+#         if number >= 0:
+#             positive_numbers.append(number)
+#     return positive_numbers
+#
+#
+# numbers = []
+# for i in range(0, 5):
+#     new_number = random.randint(-10, 10)
+#     numbers.append(new_number)
+#
+# print(numbers)
+# print(get_positive_numbers(numbers))
+
+
+certificates = ['A1', 'A2', 'B1', 'B2', 'AIH Torpaq Alqi Satqisi',\
+                'Gomruk IT']
+
+random_certificate = random.choice(certificates)
+print(random_certificate)
+
+# Random ededlerden ibaret list yaratmaq ve
+# hansisa random ededi o listde olub olmadigini yoxlamaq gerekir.
+
+
+
