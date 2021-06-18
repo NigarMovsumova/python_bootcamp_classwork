be76f4152a23c2746dfcfafbf3e7e28cc3a5cc3b
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-06-18 22:03:03 +0400

    added added bool check explanation

--- tuples.py
+++ tuples.py
@@ -56,13 +56,23 @@
 sample_tuple = tuple([1, 2, 3, 'n', True, 4.5, 5, False, 'i', 'gar', 6])
 numbers_sum = 0
 result_str = ''
-for el in sample_tuple:
-    if type(el) in [int, float]:
-        numbers_sum += el
-    elif type(el) == str:
-        result_str += el
+boolean_var = None
+# for el in sample_tuple:
+#     if type(el) in [int, float]:
+#         numbers_sum += el
+#     elif type(el) == str:
+#         result_str += el
+#     elif type(el) == bool:
+#         boolean_var = el
+
+for i in range(len(sample_tuple) - 1, 0, -1):
+    if type(sample_tuple[i]) == bool:
+        boolean_var = sample_tuple[i]
+        break
+
+print(boolean_var)
 
 
-print(numbers_sum)
-print(result_str)
+# print(numbers_sum)
+# print(result_str)
 
