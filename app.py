cd05dca7b30a5e156bfe8f80ec52813a0c3bb6fb
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-06-24 19:53:47 +0400

    Added initial explanation for modules

---
+++ app.py
@@ -0,0 +1,17 @@
+# import tkinter
+
+# from tkinter import *
+
+# import tkinter as tk
+
+# numpy, pandas -> np, ps
+
+from basket import Basket
+from tkinter import Tk
+
+root_window = Tk()
+
+basket = Basket()
+print(basket)
+root_window.mainloop()
+
---
+++ dictionary.py
@@ -0,0 +1,28 @@
+# 5. Telebelerin siyahisini silmek - Are you sure ?
+
+
+students  = {
+    'Ali': 90,
+    'Nigar': 68
+}
+
+# choice = input('''Make a choice:
+# 1. Clear dictionary
+# 2. Show all students''')
+#
+# if choice == '1':
+#     user_input = input('Are you sure? Enter \'y\' to confirm: ')
+#     if user_input == 'y':
+#         students.clear()
+#         print('Students are successfully removed.')
+# elif choice == '2':
+#     print(students)
+# else:
+#     print('There is no choice like that.')
+
+
+# test = input('Enter the input:')
+# while test != 's':
+#     print('If you want to end the program, enter s. ')
+#     test = input('Enter the input:')
+# print('Good bye!')
