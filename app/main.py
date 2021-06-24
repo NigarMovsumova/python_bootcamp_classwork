73206f32f93f5141b92de0f9f894da9139b0d7ec
movsum.nigar@gmail.com <As159951>
2021-06-24 22:25:32 +0400

    added folder practise task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
 # 1. contact classinda 3 contact yaradib, siyahinin ichine yazib chap etmek
+
+from phonebook.contact import Contact
+
+seymur = Contact("0513585588", "Seymur")
+murad = Contact("5367537634576", "Murad")
+nigar = Contact('12344', 'Nigar')
+contact_list = [seymur, murad, nigar]
+print(contact_list)
