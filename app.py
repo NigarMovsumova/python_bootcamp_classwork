748c2ba9a8ce674b7be50685ed3e6ca6e26e6fd6
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-07-01 21:16:55 +0400

    Added circular import explanation

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import test
 #
 # print(random.__name__)
 # print(test.__name__)
-name = input('Enter your name: ')
+# name = input('Enter your name: ')
 
 
 # 2 funksiya var, biri 10dan 1e kimi chap edir reqemleri, digeri ise
@@ -14,3 +14,18 @@ name = input('Enter your name: ')
 # 10dan 1e kimi chap eden funksiyani diger modulda chagirmaq gerekir.
 # 1den 10a kimi chap eden funksiyani diger modulda chagirmaq gerekir.
 # if __name__ = '__main__' - test etmek ucun
+
+from main import print_numbers_asc
+
+
+# descending
+def print_numbers_desc(num1, num2):
+    for i in range(num1, num2, -1):
+        print(i, end=',  ')
+
+
+print_numbers_asc(1, 11)
+
+if __name__ == '__main__':
+    print_numbers_desc(10, 0)
+
