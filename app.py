9d7ba1b844f4189525a36db2b91211d01b304031
Movsumova Nigar <n.movsumova@itstep.edu.az>
2021-07-01 20:02:44 +0400

    added new taskj

---
+++ app.py
@@ -0,0 +1,16 @@
+import test
+# import random
+
+# print(__name__)
+# # print(t.__name__)
+#
+# print(random.__name__)
+# print(test.__name__)
+name = input('Enter your name: ')
+
+
+# 2 funksiya var, biri 10dan 1e kimi chap edir reqemleri, digeri ise
+# 1den 10a kimi chap edir. heresi ferqli moduldadir
+# 10dan 1e kimi chap eden funksiyani diger modulda chagirmaq gerekir.
+# 1den 10a kimi chap eden funksiyani diger modulda chagirmaq gerekir.
+# if __name__ = '__main__' - test etmek ucun
