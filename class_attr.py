2199807f5a2e8775ea9e50c8ca36d8ecfe7d448d
movsum.nigar@gmail.com <As159951>
2021-07-02 21:59:54 +0400

    Added SalesManager class

--- class_attr.py
+++ class_attr.py
@@ -6,24 +6,36 @@ class Employee:
         self.salary = salary
 
     def __str__(self):
-        return f'{self.__class__.__name__}: name={self.name} ' \
-               f'salary={self.salary}'
+        return '{}: name={} salary={}'.format(
+            self.__class__.__name__, self.name, self.salary)
 
 
-class SalesManager:
-    pass
+class SalesManager(Employee):
+    base_salary = 350.00
+
+    def __init__(self, name, sales_amount):
+        self.name = name
+        self.salary = sales_amount * 0.05 + SalesManager.base_salary
+
+
+employee = SalesManager('Jan', 500)
+employee_new = SalesManager('Silvester', 500)
+print(employee)
 
 employee = Employee('Jan', 2750)
 employee_new = Employee('Silvester', 3500)
 print(employee)
-print(employee.company_name)
-print(Employee.company_name)
-Employee.company_name = 'Azercell'
-print(Employee.company_name)
-print(employee.company_name)
-print(employee_new.company_name)
-Employee.company_name = None
-print(Employee.company_name)
-print(employee.company_name)
-print(employee_new.company_name)
 
+sales_manager = SalesManager('Arnold', 100000)
+print(sales_manager)
+
+# print(employee.company_name)
+# print(Employee.company_name)
+# Employee.company_name = 'Azercell'
+# print(Employee.company_name)
+# print(employee.company_name)
+# print(employee_new.company_name)
+# Employee.company_name = None
+# print(Employee.company_name)
+# print(employee.company_name)
+# print(employee_new.company_name)
